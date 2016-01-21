160e89b72f64dc78c0532ea9644806d5aac6ad72
mhossain <mahmud.hossain@mlb.com>
2016-01-21 13:18:00 -0500

    adding reactjs tables

--- src/modules/speedtest/files/react_ctrl.js
+++ src/modules/speedtest/files/react_ctrl.js
@@ -1,104 +1,122 @@
 define(['react', 'reactDom'], function(React, ReactDOM) {
   "use strict";
   
-  var Ctrl = function(scope, log, interval, stateParams) {   
+  var Ctrl = function(scope, log, interval, stateParams) {  
+
     var rows = [], 
-        columns = [], 
-        cell = [],
-        tabs = '',
-        startInterval = '',
-        rnd = 0,
-        rgba1 = 0,
-        rgba2 = 0,
-        rgba3 = 0;
-    
-    var getRandomNumber = function(val) {
-      return Math.floor((Math.random() * val) + 0);
-    };    
-    
-    for (var i = 0; i < 51; i++) {
-      rows.push(i);
-    }
-    
-    for (var i = 0; i < 31; i++) {
-      columns.push(i);
-    }
-
-    function Create2DArray(rows) {
-      var arr = [];
-      for (var i=0; i <rows; i++) {
-         arr[i] = [];
+          columns = [], 
+          cell = [],
+          tabs = '',
+          startInterval = '',
+          rnd = 0,
+          rgba1 = 0,
+          rgba2 = 0,
+          rgba3 = 0;
+      
+      var getRandomNumber = function(val) {
+        return Math.floor((Math.random() * val) + 0);
+      };    
+      
+      for (var i = 0; i < 51; i++) {
+        rows.push(i);
       }
-      return arr;
-    }
+      
+      for (var i = 0; i < 31; i++) {
+        columns.push(i);
+      }
 
-    cell = Create2DArray(100);
-    rgba1 = Create2DArray(255);
-    rgba2 = Create2DArray(255);
-    rgba3 = Create2DArray(255);
-    
-    var intervalFn = function() {
-      return interval(function() {
-        log.debug("start timer");
-        var rndCols = getRandomNumber(31);
-        var rndRows = getRandomNumber(51);
-        cell[rndCols][rndRows] =  getRandomNumber(255);
-        rgba1[rndCols][rndRows] =  getRandomNumber(255);
-        rgba2[rndCols][rndRows] =  getRandomNumber(255);
-        rgba3[rndCols][rndRows] =  getRandomNumber(255);
-      }, 0);   
-    };  
+      function Create2DArray(rows) {
+        var arr = [];
+        for (var i=0; i <rows; i++) {
+           arr[i] = [];
+        }
+        return arr;
+      }
 
-    scope.rows = rows;
-    scope.columns = columns;
-    scope.cell = cell;
-    scope.rgba1 = rgba1;
-    scope.rgba2 = rgba2;
-    scope.rgba3 = rgba3;
-    
-    scope.startTimer = function() {
-      startInterval = intervalFn();
-    };
-    
-    scope.stopTimer = function() {
-      interval.cancel(startInterval);
-      log.debug("stop timer");
-    };   
+      cell = Create2DArray(100);
+      rgba1 = Create2DArray(255);
+      rgba2 = Create2DArray(255);
+      rgba3 = Create2DArray(255);
+      
+      var intervalFn = function() {
+        return interval(function() {
+          log.debug("start timer");
+          var rndCols = getRandomNumber(31);
+          var rndRows = getRandomNumber(51);
+          cell[rndCols][rndRows] =  getRandomNumber(255);
+          rgba1[rndCols][rndRows] =  getRandomNumber(255);
+          rgba2[rndCols][rndRows] =  getRandomNumber(255);
+          rgba3[rndCols][rndRows] =  getRandomNumber(255);
+        }, 0);   
+      };  
+      
+      var startTimer = function() {
+        startInterval = intervalFn();
+      };
+      
+      var stopTimer = function() {
+        interval.cancel(startInterval);
+        log.debug("stop timer");
+      }; 
+
 
     var CellBlock = React.createClass({
-	  render: function() {
-	  	var cellValue = this.props.data.text;
-	    return <div>{cellValue}</div>
-	  }
-	});
+      render: function() {
+        log.debug("CellBlock row: " + this.props.row);
+        log.debug("CellBlock column: " + this.props.column);
+        return (
+          <div></div>
+        );
+      }
+  });
+
 
     var ColumnGrid = React.createClass({
-	  render: function() {
-	    return (
-	    	<div className="col-md-1 thumbnail inline-center">
-		  		{columns.map(function(column) {
-					return <CellBlock data="{column}" />
-		  		})}
-	  		</div>
-    	);
-	  }
-	});
+      render: function() {
+        log.debug("ColumnGrid info: " + this.props.info);
+        return (
+          <div className="row">
+            {columns.map(function(column, id) {
+               return (
+                <div className="col-md-1 thumbnail inline-center">
+                  <CellBlock column="{column}" row="{this.props.row}" />
+                </div>
+              );
+            })}
+          </div>
+        );
+      }
+  });
 
     var RowGrid = React.createClass({
-	  render: function() {
-	  	console.log("rows: " + this.props.rows);
-	  	console.log("columns: " + this.props.columns);
-	  	// var rowList = rows.map(function(row) {
-	  	// 	return <ColumnGrid  />
-	  	// });
-	    return <div className="row">{this.props.rows[0]}{this.props.columns[0]}</div>;
-	  }
+  	  render: function() {     
+  	  	var rowList = rows.map(function(row, id) {
+          return <ColumnGrid info="{row}" />
+  	  	});
+  	    return <span>{rowList}</span>;
+  	  }
 	});
-	
-	ReactDOM.render(
-	  <RowGrid rows="rows" columns="{columns}" />,
-	  document.getElementById('react')
-	);
+  
+  ReactDOM.render(
+    <RowGrid />,
+    document.getElementById('table1')
+  );
+  ReactDOM.render(
+    <RowGrid />,
+    document.getElementById('table2')
+  );
+  ReactDOM.render(
+    <RowGrid />,
+    document.getElementById('table3')
+  );
+  ReactDOM.render(
+    <RowGrid />,
+    document.getElementById('table4')
+  );
+  ReactDOM.render(
+    <RowGrid />,
+    document.getElementById('table5')
+  );
 
   };
 
